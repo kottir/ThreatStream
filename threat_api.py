a3b7b27892e71e846dc143f928804610ac8c9cc8
kottir <kotti.r@husky.neu.edu>
2015-11-17 19:18:25 -0500

    few comments added to the threat_api file

--- threat_api.py
+++ threat_api.py
@@ -5,6 +5,7 @@ import argparse
 import csv
 import re
 
+# arguments for the tool
 parser= argparse.ArgumentParser(description='desc')
 parser.add_argument("-t","--target", nargs='*',help="Target IP")
 parser.add_argument("-k","--key",nargs='?',help="API key")
@@ -14,21 +15,35 @@ args=parser.parse_args()
 
 '''apiuser=''
 apikey=''
-'''
+uncomment the above 2 lines if you want to hardcode the values'''
+
 query_api_url="https://api.threatstream.com/api/v2"
 resource='intelligence'
+apiuser= args.user
+apikey= args.key
 
-def callurl(urltouse,pos):
+'''
+function to send out the GET request.
+urltouse= variable carrying the appropriate url.
+pos_target indicates the position of the target if multiple given.
+output_obj= object for the OutputTypes class within the outputs file.
+
+Based on the number of the targets entered, the appropriate method in the outputs
+file is chosen.
+
+checks the status code to determine the action to be conducted next.
+'''
+def callurl(urltouse,pos_target):
 	try:
 		http_req=requests.get(urltouse, headers={'ACCEPT':'application/json, text/html'})
 		if http_req.status_code==200:
 			rblob=http_req.json()['objects']
-			if args.csv and pos==0:
+			if args.csv and pos_target==0:
 				output_obj= OutputTypes(args.csv, rblob)
 				output_obj.csvoutfile()
-			elif args.csv and pos>0:
+			elif args.csv and pos_target>0:
 				output_obj=OutputTypes(args.csv, rblob)
-				output_obj.csvoutfile()
+				output_obj.csvoutfile1()
 		elif http_req.status_code == 401:
 			print 'Check API credentials, access has been denied'
 		else:
@@ -37,12 +52,22 @@ def callurl(urltouse,pos):
 		print 'API access error: {}'.format(err)
 		exit(0)
 
+'''
+collecting the targets entered.
+list used as multiple can be entered
+'''
+
 ips=[]
 if len(args.target)>1:
 	for i in range(0,len(args.target)):
 		ips.append(args.target[i])
 else:
 	ips.append(args.target[0])
+
+'''
+Regular expressions being used to determine if the entered target value is an IP or a CIDR.
+The URL to be used changes based on the what the target is
+'''
 
 for j in range(0, len(ips)):
 	ipregfind=re.search("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",ips[j])
